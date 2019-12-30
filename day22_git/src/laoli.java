56dea03ca21ddc333e95cd68999e4a69fc1823a3
hechunl <1092696013@qq.com>
2019-12-30 19:03:37 +0800

    第一次修改

--- day22_git/src/laoli.java
+++ day22_git/src/laoli.java
@@ -2,5 +2,6 @@ public class laoli {
     public static void main(String[] args) {
         System.out.println("我是何春l");
         System.out.println("我是鲁林鑫");
+        System.out.println("第一次修改");
     }
 }
